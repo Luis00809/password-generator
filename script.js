6d85c677bc7cbbb4df5f11260c60ef94f0226c92
Luis00809 <davidcarvajal008@gmail.com>
2023-06-30 13:27:50 -0700

    file created with tutor

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let length;
+const upCaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+let word = 'adklfhsdkghghghghgh';
+console.log(word[0]);
+
+let wordUpper = word.toUpperCase();
+console.log(wordUpper);
+
+let charBank = '';
 
 // here I will define generatePassword function
 function generatePassword() {
-// does length ===  the string inputed by the user? or is the value of length the prompt?
-
 
 
 // use a do-while loop, in a nested function, to prompt for a number and reject a value less than min and greater than max
 
 function promptLength(min, max) {
-  let length;
+  length;
   do {
     length = prompt('How many characters do you want to contain in your password?\nChose a number between 8 and 128');
   
   } while (isNaN(length) || parseInt(length, 10) < 8 || parseInt(length, 10) > 128);
   return parseInt(length, 10);
+  
 }
+
  let passwordLength = promptLength(8, 128)
+ console.log(passwordLength);
 
 // now we have the desired length
 // works in the console
@@ -39,10 +49,14 @@ const lowCaseletters = (() => {
 })();
 
 if(low) {
-  // generate random selection of lowercase letters
-let lowLettersIndex = Math.floor(Math.random() * lowCaseletters.length);
-let lowLettersDisplay = lowCaseletters[lowLettersIndex];
-console.log(lowLettersDisplay);
+
+console.log('before: ' + charBank);
+charBank = charBank + word;
+console.log('after', charBank );
+console.log('after ' + charBank );
+// let lowLettersIndex = Math.floor(Math.random() * lowCaseletters.length);
+// let lowLettersDisplay = lowCaseletters[lowLettersIndex];
+// console.log(lowLettersDisplay);
 // works in the console 
 }
 
@@ -51,8 +65,6 @@ console.log(lowLettersDisplay);
 
 
 let upper = confirm('Include uppercase letters?');
-
-const upCaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 if(upper) {
   let upIndexLetters = Math.floor(Math.random() * upCaseletters.length);
@@ -67,23 +79,30 @@ if(upper) {
 let integer = confirm('include a numeric value?');
 const intOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 if(integer) {
-  let intIndex = Math.floor( Math.random() * intOptions.length);
-  let intDisplay = intOptions[intIndex];
-  console.log(intDisplay);
+  console.log('before: ' + charBank);
+  charBank = charBank + intOptions;
+  console.log('after: ' + charBank);
+  // let intIndex = Math.floor( Math.random() * intOptions.length);
+  // let intDisplay = intOptions[intIndex];
+  // console.log(intDisplay);
+}
+for(let i = 0; i < passwordLength; i++) {
+  console.log(charBank[i]);
 }
 
 
+// let special = confirm('Include special characters?');
+// const specialList = ['!', '”', '#', '$', '%', '&', "'", '(', ')',
+//  '*', '+', ',', '-', '.', '/', ';', ':', '<', '=', '>', '?', '@', 
+//  '[', '\ fix?', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-let special = confirm('Include special characters?');
-const specialList = ['!', '”', '#', '$', '%', '&', "'", '(', ')',
- '*', '+', ',', '-', '.', '/', ';', ':', '<', '=', '>', '?', '@', 
- '[', '\ fix?', ']', '^', '_', '`', '{', '|', '}', '~'];
+// if(special) {
+//   let specialIndex = Math.floor(math.random() * specialList.length);
+//   let specialDisplay = specialList[specialIndex];
+//   console.log(specialDisplay);
+// // function runs when i comment out snippet for special characters
+// }
 
-if(special) {
-  let specialIndex = Math.floor(math.random() * specialList.length);
-  let specialDisplay = specialList[specialIndex];
-  console.log(specialDisplay);
-// function runs when i comment out snippet for special characters
 }
 
 
